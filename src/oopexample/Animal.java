7800d4edd55e1614798213a0de555ff170eeb2e1
nirajMudbari <niraaz2010@gmail.com>
2020-02-12 09:01:40 +0545

    animal

--- src/oopexample/Animal.java
+++ src/oopexample/Animal.java
@@ -1,9 +1,32 @@
 package oopexample;
 
 public class Animal {
+	String name;
+	int age;
+	String color;
+	
+	Animal(String n,int a,String c){
+		name=n;
+		age=a;
+		color=c;
+	}
+	
+	Animal(){
+		
+	}
+	
+    void display(Animal a) {
+		System.out.println("The name of animal is "+a.name +"age is "+a.age+"color is"+a.color);
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Animal animal=new Animal(); //default constructor
+		animal.name="cat";
+		animal.age=2;
+		animal.color="black";
+		animal.display(animal);
+		Animal animal1=new Animal("Dog",4,"grey");
+		animal1.display(animal1);
 
 	}
 
